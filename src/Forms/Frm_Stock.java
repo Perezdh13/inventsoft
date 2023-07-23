c620f8cc68a6a1e95dbc3b476c4bb4e29902b2fe
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-07-23 23:39:35 +0200

    creado formulario para añadir elementos al stock y metodo para insertar el elemento al archivo xml del sistema

--- src/Forms/Frm_Stock.java
+++ src/Forms/Frm_Stock.java
@@ -1,20 +1,30 @@
 package Forms;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Frm_Stock{
     public JPanel stock_JPanel;
     public JTable stockTable;
-    public JButton exitButton;
-    public JButton button1;
-    public JButton button2;
+    public JButton deleteItem_Button;
+    public JButton newItem_button;
+    public JButton editItem_button;
 
 
     public Frm_Stock(){
 
+        newItem_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Frm_InsertItem frmInsertItem = new Frm_InsertItem();
+                JFrame insertItemFrame = new JFrame("Añadir Item");
+                insertItemFrame.setContentPane(frmInsertItem.insertItem);
+                insertItemFrame.setSize(850,150);
+                insertItemFrame.setLocationRelativeTo(null);
+                insertItemFrame.setVisible(true);
+            }
+        });
     }
 
 
